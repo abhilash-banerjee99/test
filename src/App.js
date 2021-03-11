bb30f256433df6c77aed45ad742120e6b9ff1d5b
Abhilash <52290060+abhilash-banerjee99@users.noreply.github.com>
2021-03-11 09:03:30 -0800

    Add users and post component in sidebar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import * as React from "react";
 import { Admin, Resource, ListGuesser } from "react-admin";
 import jsonServerProvider from "ra-data-json-server";
-
+import {UserList} from './posts';
 const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
 
 const App = () => (
   <Admin dataProvider={dataProvider}>
-    <Resource name="users" list={ListGuesser} />
+    <Resource name="users" list={UserList}/>
+    <Resource name="posts" list={ListGuesser} />
   </Admin>
 );
 
