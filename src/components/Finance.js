ab7684d1ab5fba8649d1a53d81f5b694936c9ec3
Abhilash <52290060+abhilash-banerjee99@users.noreply.github.com>
2021-03-27 08:34:59 -0700

    Add sales revenue charts using recharts

--- src/components/Finance.js
+++ src/components/Finance.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {makeStyles} from '@material-ui/core/styles';
-import {Paper,Divider,CardHeader, Typography, Breadcrumbs, Link} from '@material-ui/core'
+import {Paper,Divider,CardHeader, CardContent, Typography, Breadcrumbs, Link} from '@material-ui/core'
+import {LineChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Line} from 'recharts'
 import TimelineIcon from '@material-ui/icons/Timeline';
 
 const useStyles = makeStyles((theme)=>({
@@ -20,6 +21,80 @@ const useStyles = makeStyles((theme)=>({
     
   }))
 
+  const data = [
+    {
+      "name": "JAN",
+      "New Customer": 31,
+      "Up/Cross-Selling": 11,
+    //   "amt": 2400
+    },
+    {
+      "name": "FEB",
+      "New Customer": 40,
+      "Up/Cross-Selling": 32,
+    //   "amt": 2210
+    },
+    {
+      "name": "MAR",
+      "New Customer": 28,
+      "Up/Cross-Selling": 45,
+      //"amt": 2290
+    },
+    {
+      "name": "APR",
+      "New Customer": 51,
+      "Up/Cross-Selling": 32,
+      //"amt": 2000
+    },
+    {
+      "name": "MAY",
+      "New Customer": 42,
+      "Up/Cross-Selling": 34,
+    //   "amt": 2181
+    },
+    {
+      "name": "JUN",
+      "New Customer": 109,
+      "Up/Cross-Selling": 52,
+    //   "amt": 2500
+    },
+    {
+      "name": "JUL",
+      "New Customer": 100,
+      "Up/Cross-Selling": 41,
+    //   "amt": 2100
+    },
+    {
+        "name": "AUG",
+        "New Customer": 120,
+        "Up/Cross-Selling": 80,
+        // "amt": 2100
+    },
+    {
+        "name": "SEP",
+        "New Customer": 80,
+        "Up/Cross-Selling": 96,
+        // "amt": 2100
+    },
+    {
+        "name": "OCT",
+        "New Customer": 42,
+        "Up/Cross-Selling": 140,
+        // "amt": 2100
+    },
+    {
+        "name": "NOV",
+        "New Customer": 90,
+        "Up/Cross-Selling": 30,
+        // "amt": 2100
+    },
+    {
+        "name": "DEC",
+        "New Customer": 140,
+        "Up/Cross-Selling": 100,
+        // "amt": 2100
+    }
+  ]
 export default function FinanceData() {
     const classes = useStyles();
     function handleClick(event){
@@ -73,12 +148,25 @@ export default function FinanceData() {
                     </div>
                     </Paper>
                 </div>
+                <br/>
                 <Paper className={classes.paper}>
                     <CardHeader 
                         title={
                             <Typography variant='span'>Sales Revenue</Typography>
                         }
                     />
+                    <CardContent>
+                    <LineChart width={730} height={250} data={data}
+                    margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+                        <CartesianGrid strokeDasharray="1 1" />
+                        <XAxis dataKey="name" />
+                        <YAxis />
+                        <Tooltip />
+                        <Legend />
+                        <Line type="monotone" dataKey="New Customer" stroke="blue" />
+                        <Line type="monotone" dataKey="Up/Cross-Selling" stroke="purple" />
+                    </LineChart>
+                    </CardContent>
                 </Paper>
                 </div>
         </div>
